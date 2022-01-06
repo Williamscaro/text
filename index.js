c0a02fa6fe7e0fe611905d622b2f4a1183abc410
Williams <williamscaro194@gmail.com>
2022-01-06 10:58:04 -0400

    Variables listo ahora vamos a practicar un poco de constantes

--- index.js
+++ index.js
@@ -23,3 +23,18 @@ height = 180;
 // Aquí podemos notar que nuestra variable llamada height tiene almacenado 180
 console.log(height); // -> 180        si imprimimos esto nos dice que tenemos 180,
 console.log("height"); // -> height       en cambio esto lo toma literal por las  comillas e imprime height.
+
+//almacenar string luego cambiar su valor y tipo
+let greeting = "Hello!"; //Aquí hemos almacenado un string
+let counter = 100; //mientras que aquí hemos almacenado un valor entero
+
+console.log(greeting);
+greeting = 1;
+console.log(greeting);
+//Aquí podemos apreciar que en JS podemos declarar una variable que almacena un tipo de dato y luego cambiarlo y no afecta.
+
+//conversión implicita
+let greeting = "Hello!";
+let counter = 100;
+greeting = greeting + counter;
+console.log(greeting); // -> Hello!100  conversión implicitá JS encontró a greeting y luegó como hicimos una concatenación lo que hizo fue una conversión implicita y convirtio a counter en string.
